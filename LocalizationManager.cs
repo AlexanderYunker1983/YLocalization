543cd44bdab121f05522c11d1834b68cdfce8c81
alexy <alexy@local.yurion.ru>
2023-06-03 12:29:45 +0300

    Проект локализации

---
+++ LocalizationManager.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace YLocalization
+{
+    public class LocalizationManager : ILocalizationManager
+    {
+        /// <summary>
+        /// Cached ResourceManagers for each ResourceSet supported requested.       
+        /// </summary>
+        private readonly List<ResourceManager> _resourceManagers = new List<ResourceManager>();
+
+        private CultureInfo _culture;
+        public void AddAssembly(string assemblyName, string resourcePath = "Resources.LocalizableResources")
+        {
+            var assembly = Assembly.Load(new AssemblyName(assemblyName));
+            AddAssembly(assembly, resourcePath);
+        }
+
+        public event EventHandler CultureChanged;
+
+        public CultureInfo Culture
+        {
+            get => _culture;
+            set
+            {
+                if (!Equals(_culture, value))
+                {
+                    _culture = value;
+                    CultureChanged?.Invoke(this, EventArgs.Empty);
+                }
+
+            }
+        }
+
+        public virtual void ChangeCulture(CultureInfo cultureInfo)
+        {
+            Culture = cultureInfo;
+        }
+
+        public string GetString(string key, params object[] parameters)
+        {
+            foreach (var resourceManager in _resourceManagers)
+            {
+                var str = GetString(resourceManager, key, Culture, parameters);
+                if (!string.IsNullOrEmpty(str)) return str;
+            }
+            return $"?{key}?";
+        }
+
+        private static string GetString(ResourceManager manager, string stringName, CultureInfo cultureInfo, params object[] parameters)
+        {
+            if (manager == null) return null;
+            string str;
+            var unFormattedString = string.Empty;
+            try
+            {
+                unFormattedString = manager.GetString(stringName, cultureInfo) ?? String.Empty;
+            }
+            catch (MissingManifestResourceException)
+            {
+            }
+            catch (NullReferenceException)
+            {
+            }
+            try
+            {
+                str = string.Format(unFormattedString, parameters);
+            }
+            catch (FormatException)
+            {
+                str = unFormattedString;
+            }
+            return str;
+        }
+
+        public void AddResourceManager(ResourceManager resourceManager)
+        {
+            if (resourceManager != null) _resourceManagers.Add(resourceManager);
+        }
+
+        public void AddAssembly(Assembly assembly, string resourcePath = "Resources.LocalizableResources")
+        {
+            var resName = GetDefaultResourceName(assembly.ManifestModule.Name, resourcePath);
+            _resourceManagers.Add(new ResourceManager(resName, assembly));
+        }
+        private static string GetDefaultResourceName(string assemblyModuleName, string resourcePath)
+        {
+            string stringResourceName;
+            if (assemblyModuleName.ToLower().Contains(".exe"))
+            {
+                stringResourceName = $"{assemblyModuleName.Remove(assemblyModuleName.ToLower().LastIndexOf(".exe", StringComparison.Ordinal))}.{resourcePath}";
+                return stringResourceName;
+            }
+            stringResourceName = $"{assemblyModuleName.Remove(assemblyModuleName.ToLower().LastIndexOf(".dll", StringComparison.Ordinal))}.{resourcePath}";
+            return stringResourceName;
+        }
+
+    }
+}
---
+++ LocalizedEnumWrapper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YLocalization
+{
+    public class LocalizedEnumWrapper<T>
+    {
+        public LocalizedEnumWrapper(ILocalizationManager localizationManager, T enumValue)
+        {
+            Value = enumValue;
+            var key = EnumToResourceKey(enumValue);
+            Text = localizationManager.GetString(key);
+        }
+
+        public string StringValue => Value?.ToString();
+        public T Value { get; }
+
+        public string Text { get; set; }
+
+        public static List<LocalizedEnumWrapper<T>> GetValues(ILocalizationManager localizationManager, bool firstValueToLast = false)
+        {
+            var enumValues = Enum.GetValues(typeof(T)).Cast<T>().ToList();
+            var result = new List<LocalizedEnumWrapper<T>>();
+            if (firstValueToLast && enumValues.Count > 1)
+            {
+                result.AddRange(enumValues.Skip(1).Select(arg => new LocalizedEnumWrapper<T>(localizationManager, arg)));
+                result.Add(new LocalizedEnumWrapper<T>(localizationManager, enumValues.First()));
+            }
+            else
+            {
+                result = enumValues.Select(arg => new LocalizedEnumWrapper<T>(localizationManager, arg)).ToList();
+            }
+            return result;
+        }
+
+        private bool Equals(LocalizedEnumWrapper<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((LocalizedEnumWrapper<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(Value);
+        }
+
+        public static bool operator ==(LocalizedEnumWrapper<T> left, LocalizedEnumWrapper<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(LocalizedEnumWrapper<T> left, LocalizedEnumWrapper<T> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public static string EnumToResourceKey(T enumValue)
+        {
+            var enumType = enumValue.GetType();
+            var key = $"{enumType.Name}_{Enum.GetName(enumType, enumValue)}";
+            return key;
+        }
+    }
+}
